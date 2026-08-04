72eb2fc10ca41de1faa02c241bac2907df457b04
angpe995 <angpe995@gmail.com>
2026-08-04 10:53:54 +0200

    style: apply consistent code formatting

--- services/availabilityService.js
+++ services/availabilityService.js
@@ -1,58 +1,69 @@
 const { getAccessToken } = require("./authService");
-const {getConnectionPrice} = require("./trainService");
+const { getConnectionPrice } = require("./trainService");
 const api = require("./pkpApi");
-const fetchSeatsAvailability  = async (connectionId,trainId, seatClass) => {
-    const token = await getAccessToken();
-    const response = await api.get(`/seats_availability/${connectionId}/${trainId}/${seatClass}`,{headers: {
-        Authorization: `Bearer ${token}`
-    }});
-    return response.data;
+const fetchSeatsAvailability = async (connectionId, trainId, seatClass) => {
+  const token = await getAccessToken();
+  const response = await api.get(
+    `/seats_availability/${connectionId}/${trainId}/${seatClass}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return response.data;
 };
-const getConnectionId=async(uuid)=>{
-    const token = await getAccessToken();
-    const response = await api.put(`/eol_connections/${uuid}/connection_id`,{headers: {
-        Authorization: `Bearer ${token}`}});
-    return response.data.connection_id;
+const getConnectionId = async (uuid) => {
+  const token = await getAccessToken();
+  const response = await api.put(`/eol_connections/${uuid}/connection_id`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data.connection_id;
 };
 const getFreeSeats = (seats) => {
-    return seats.filter(seat => seat.state === "FREE");
+  return seats.filter((seat) => seat.state === "FREE");
 };
-const checkWholeConnection = async (
-    connection, placeTypeIds)=> {
-    if (!connection.legs?.length) {
-        return [];
+const checkWholeConnection = async (connection, placeTypeIds) => {
+  if (!connection.legs?.length) {
+    return [];
+  }
+  const results = [];
+  const connectionId = await getConnectionId(connection.uuid);
+  let index = 0;
+  for (const leg of connection.legs) {
+    if (leg.leg_type !== "train_leg") {
+      continue;
     }
-    const results = [];
-    const connectionId = await getConnectionId(connection.uuid);
-    let index = 0;
-    for (const leg of connection.legs) {
-        if (leg.leg_type !== "train_leg") {
-        continue;
+    let placeTypes = [];
+    for (const placeTypeId of placeTypeIds) {
+      try {
+        const seats = await fetchSeatsAvailability(
+          connectionId,
+          leg.train_nr,
+          placeTypeId,
+        );
+        placeTypes.push({ id: placeTypeId, seats: getFreeSeats(seats.seats) });
+      } catch (e) {
+        if (e.response?.status === 422) {
+          continue;
         }
-        let placeTypes = [];
-        for (const placeTypeId of placeTypeIds) {
-        try {
-            const seats = await fetchSeatsAvailability(connectionId,leg.train_nr,placeTypeId);
-            placeTypes.push({id: placeTypeId,seats: getFreeSeats(seats.seats),});
-        } catch (e) {
-            if (e.response?.status === 422) {
-            continue;
-            }
-        }
-        }
-        results.push({
-            train_nr: leg.train_nr,
-            origin_station_id: leg.origin_station_id,
-            destination_station_id: leg.destination_station_id,
-            place_types: placeTypes,
-        });
-        index++;
+      }
     }
-    return results;
+    results.push({
+      train_nr: leg.train_nr,
+      origin_station_id: leg.origin_station_id,
+      destination_station_id: leg.destination_station_id,
+      place_types: placeTypes,
+    });
+    index++;
+  }
+  return results;
 };
 module.exports = {
-    checkWholeConnection,
-    getFreeSeats,
-    getConnectionId,
-    fetchSeatsAvailability,
-}
+  checkWholeConnection,
+  getFreeSeats,
+  getConnectionId,
+  fetchSeatsAvailability,
+};
--- services/trainService.js
+++ services/trainService.js
@@ -2,81 +2,85 @@ const api = require("./pkpApi");
 const { getAccessToken } = require("./authService");
 
 const getConnections = async (date, start, end) => {
-    try{
-        const response = await api.post(
-        "/eol_connections/search",
-        {
-            start_id: start,
-            end_id: end,
-            departure_after: date,
-            only_direct: false
-        });
-        return response.data;
-    }catch(e){
-        if(e.response?.status === 401){
-            throw new Error("Failed to get connections from PKP API: "+ e.response.data.message);
-        }
-        throw new Error("Failed to get connections from PKP API: " + e.message);
-    } 
-};
-const getConnectionPrice =async(connection_id) =>{
-    try{
-        const result=await api.get(`/connections/${connection_id}/price?context=traveloptions`)
-        return result.data.prices[0];
-    } catch(e)
-    {
-        if (e.response?.status === 404) {
-            return null;
-        }
-        console.error(e.message);
-        console.log(e.response?.status);
-        console.log(e.response?.data);
-        throw new Error("Failed to get connection price.");
+  try {
+    const response = await api.post("/eol_connections/search", {
+      start_id: start,
+      end_id: end,
+      departure_after: date,
+      only_direct: false,
+    });
+    return response.data;
+  } catch (e) {
+    if (e.response?.status === 401) {
+      throw new Error(
+        "Failed to get connections from PKP API: " + e.response.data.message,
+      );
     }
+    throw new Error("Failed to get connections from PKP API: " + e.message);
+  }
 };
-const getTariffids=async(connection_id)=>{
-    const respone = await getConnectionPrice(connection_id);
-    if (!respone) {
-        return [];
+const getConnectionPrice = async (connection_id) => {
+  try {
+    const result = await api.get(
+      `/connections/${connection_id}/price?context=traveloptions`,
+    );
+    return result.data.prices[0];
+  } catch (e) {
+    if (e.response?.status === 404) {
+      return null;
     }
-    return respone.tariff_ids;
+    console.error(e.message);
+    console.log(e.response?.status);
+    console.log(e.response?.data);
+    throw new Error("Failed to get connection price.");
+  }
 };
-const getNestedSeats=async(connectionID,tariffId)=>{
-     try
-     {
-        const token = await getAccessToken();
-        const response = await api.post(
-            `/nested_train_place_types/${connectionID}`,   
-            {
-                tariff_ids: tariffId
-            },
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        return response.data;
-    } catch (e) {
-        console.error(e.message);
-        if (e.response?.status === 422) {
-            return {
-                train_place_types: []
-            };
-        }
-    }   
+const getTariffids = async (connection_id) => {
+  const respone = await getConnectionPrice(connection_id);
+  if (!respone) {
+    return [];
+  }
+  return respone.tariff_ids;
 };
-const getPlaceTypes=async(connectionID,tariffId)=>{
-    const nestedSeats = await getNestedSeats(connectionID,tariffId);
-    if (!nestedSeats?.train_place_types?.length) {
-        return [];
+const getNestedSeats = async (connectionID, tariffId) => {
+  try {
+    const token = await getAccessToken();
+    const response = await api.post(
+      `/nested_train_place_types/${connectionID}`,
+      {
+        tariff_ids: tariffId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (e) {
+    console.error(e.message);
+    if (e.response?.status === 422) {
+      return {
+        train_place_types: [],
+      };
     }
-    const placeTypes = nestedSeats.train_place_types.map((train) => ({train_nr: train.train_nr,placeTypes: train.place_type?.place_types?.[0]?.place_types ?? [],}));
-    return placeTypes;
+  }
 };
-module.exports={
-    getConnections,
-    getConnectionPrice,
-    getTariffids,
-    getNestedSeats,
-    getPlaceTypes
-}
+const getPlaceTypes = async (connectionID, tariffId) => {
+  const nestedSeats = await getNestedSeats(connectionID, tariffId);
+  if (!nestedSeats?.train_place_types?.length) {
+    return [];
+  }
+  const placeTypes = nestedSeats.train_place_types.map((train) => ({
+    train_nr: train.train_nr,
+    placeTypes: train.place_type?.place_types?.[0]?.place_types ?? [],
+  }));
+  return placeTypes;
+};
+module.exports = {
+  getConnections,
+  getConnectionPrice,
+  getTariffids,
+  getNestedSeats,
+  getPlaceTypes,
+};
